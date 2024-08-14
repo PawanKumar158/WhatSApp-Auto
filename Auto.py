26d5d5d96413d42071fe164252de452634f62f16
Pawan Kumar Gautam <90699283+PawanKumar158@users.noreply.github.com>
2024-08-14 23:21:57 +0530

    Update Auto.py 1

--- Auto.py
+++ Auto.py
@@ -1,16 +1,6 @@
 import pywhatkit as kit
 import time
-# import schedule
 
-# def send_whatsapp_message():
-#     kit.sendwhatmsg("+1234567890", "How are you", 22, 0)
-
-# # Schedule the message to be sent every day at 10 PM
-# schedule.every().day.at("22:00").do(send_whatsapp_message)
-
-# # Keep the script running
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
-
-kit.sendwhatmsg("+91 88866 29738","Hello, How are you?",23,9)
+# Adding a delay before sending the message
+time.sleep(5)
+kit.sendwhatmsg("+91 88866 29738", "Hello, How are you?", 23, 9)
